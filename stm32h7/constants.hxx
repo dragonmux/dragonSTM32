f51ca3769fd625c0c406111b565d4f4a864b62ea
dragonmux <git@dragonmux.network>
2024-06-15 21:25:50 -0700

    stm32h7/constants: Defined the peripheral clock source selection constants

--- stm32h7/constants.hxx
+++ stm32h7/constants.hxx
@@ -103,6 +103,156 @@ namespace vals
 
 		constexpr inline uint32_t domain3ConfigAPB4Prescaler(const uint8_t prescaler) noexcept
 			{ return static_cast<uint32_t>(prescaler) << 4U; }
+
+		// Domain 1 peripheral clock configuration register constants
+		constexpr static uint32_t domain1ClockConfigFMCMask{0x00000003U};
+		constexpr static uint32_t domain1ClockConfigFMCHClk3{0x00000000U};
+		constexpr static uint32_t domain1ClockConfigFMCPLL1Q{0x00000001U};
+		constexpr static uint32_t domain1ClockConfigFMCPLL2R{0x00000002U};
+		constexpr static uint32_t domain1ClockConfigFMCPerClk{0x00000003U};
+		constexpr static uint32_t domain1ClockConfigOctoSPIMask{0x00000030U};
+		constexpr static uint32_t domain1ClockConfigOctoSPIHClk3{0x00000000U};
+		constexpr static uint32_t domain1ClockConfigOctoSPIPLL1Q{0x00000010U};
+		constexpr static uint32_t domain1ClockConfigOctoSPIPLL2R{0x00000020U};
+		constexpr static uint32_t domain1ClockConfigOctoSPIPerClk{0x00000030U};
+		constexpr static uint32_t domain1ClockConfigSDMMCMask{0x00010000U};
+		constexpr static uint32_t domain1ClockConfigSDMMCPLL1Q{0x00000000U};
+		constexpr static uint32_t domain1ClockConfigSDMMCPLL2R{0x00010000U};
+		constexpr static uint32_t domain1ClockConfigPerClkMask{0x30000000U};
+		constexpr static uint32_t domain1ClockConfigPerClkHSI{0x00000000U};
+		constexpr static uint32_t domain1ClockConfigPerClkCSI{0x10000000U};
+		constexpr static uint32_t domain1ClockConfigPerClkHSE{0x20000000U};
+
+		// Domain 2 peripheral clock configuration register 1 constants
+		constexpr static uint32_t domain2ClockConfig1SAI1Mask{0x00000007U};
+		constexpr static uint32_t domain2ClockConfig1SAI1PLL1Q{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1SAI1PLL2P{0x00000001U};
+		constexpr static uint32_t domain2ClockConfig1SAI1PLL3P{0x00000002U};
+		constexpr static uint32_t domain2ClockConfig1SAI1I2SClk{0x00000003U};
+		constexpr static uint32_t domain2ClockConfig1SAI1PerClk{0x00000004U};
+		constexpr static uint32_t domain2ClockConfig1SPI123Mask{0x00007000U};
+		constexpr static uint32_t domain2ClockConfig1SPI123PLL1Q{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1SPI123PLL2P{0x00001000U};
+		constexpr static uint32_t domain2ClockConfig1SPI123PLL3P{0x00002000U};
+		constexpr static uint32_t domain2ClockConfig1SPI123I2SClk{0x00003000U};
+		constexpr static uint32_t domain2ClockConfig1SPI123PerClk{0x00004000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45Mask{0x00070000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45APBClk{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45PLL2Q{0x00010000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45PLL3Q{0x00020000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45HSI{0x00030000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45CSI{0x00040000U};
+		constexpr static uint32_t domain2ClockConfig1SPI45HSE{0x00050000U};
+		constexpr static uint32_t domain2ClockConfig1SPDIFMask{0x00300000U};
+		constexpr static uint32_t domain2ClockConfig1SPDIFPLL1Q{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1SPDIFPLL2R{0x00100000U};
+		constexpr static uint32_t domain2ClockConfig1SPDIFPLL3R{0x00200000U};
+		constexpr static uint32_t domain2ClockConfig1SPDIFHSI{0x00300000U};
+		constexpr static uint32_t domain2ClockConfig1DFSDM1Mask{0x01000000U};
+		constexpr static uint32_t domain2ClockConfig1DFSDM1PClk2{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1DFSDM1SysClk{0x01000000U};
+		constexpr static uint32_t domain2ClockConfig1FDCANMask{0x30000000U};
+		constexpr static uint32_t domain2ClockConfig1FDCANHSE{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1FDCANPLL1Q{0x10000000U};
+		constexpr static uint32_t domain2ClockConfig1FDCANPLL2Q{0x20000000U};
+		constexpr static uint32_t domain2ClockConfig1SWPMIMask{0x80000000U};
+		constexpr static uint32_t domain2ClockConfig1SWPMIPClk{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig1SWPMIHSI{0x80000000U};
+
+		// Domain 2 peripheral clock configuration register 2 constants
+		constexpr static uint32_t domain2ClockConfig2USART234578Mask{0x00000007U};
+		constexpr static uint32_t domain2ClockConfig2USART234578PClk1{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2USART234578PLL2Q{0x00000001U};
+		constexpr static uint32_t domain2ClockConfig2USART234578PLL3Q{0x00000002U};
+		constexpr static uint32_t domain2ClockConfig2USART234578HSI{0x00000003U};
+		constexpr static uint32_t domain2ClockConfig2USART234578CSI{0x00000004U};
+		constexpr static uint32_t domain2ClockConfig2USART234578LSE{0x00000005U};
+		constexpr static uint32_t domain2ClockConfig2USART16910Mask{0x00000038U};
+		constexpr static uint32_t domain2ClockConfig2USART16910PClk2{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2USART16910PLL2Q{0x00000008U};
+		constexpr static uint32_t domain2ClockConfig2USART16910PLL3Q{0x00000010U};
+		constexpr static uint32_t domain2ClockConfig2USART16910HSI{0x00000018U};
+		constexpr static uint32_t domain2ClockConfig2USART16910CSI{0x00000020U};
+		constexpr static uint32_t domain2ClockConfig2USART16910LSE{0x00000028U};
+		constexpr static uint32_t domain2ClockConfig2RNGMask{0x00000300U};
+		constexpr static uint32_t domain2ClockConfig2RNGHSI8{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2RNGPLL1Q{0x00000100U};
+		constexpr static uint32_t domain2ClockConfig2RNGLSE{0x00000200U};
+		constexpr static uint32_t domain2ClockConfig2RNGLSI{0x00000300U};
+		constexpr static uint32_t domain2ClockConfig2I2C1235Mask{0x00003000U};
+		constexpr static uint32_t domain2ClockConfig2I2C1235PClk1{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2I2C1235PLL3R{0x00001000U};
+		constexpr static uint32_t domain2ClockConfig2I2C1235HSI{0x00002000U};
+		constexpr static uint32_t domain2ClockConfig2I2C1235CSI{0x00003000U};
+		constexpr static uint32_t domain2ClockConfig2USBMask{0x00300000U};
+		constexpr static uint32_t domain2ClockConfig2USBPLL1Q{0x00100000U};
+		constexpr static uint32_t domain2ClockConfig2USBPLL3Q{0x00200000U};
+		constexpr static uint32_t domain2ClockConfig2USBHSI48{0x00300000U};
+		constexpr static uint32_t domain2ClockConfig2HDMI_CECMask{0x00c00000U};
+		constexpr static uint32_t domain2ClockConfig2HDMI_CECLSE{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2HDMI_CECLSI{0x00400000U};
+		constexpr static uint32_t domain2ClockConfig2HDMI_CECCSI{0x00800000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1Mask{0x70000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1PClk1{0x00000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1PLL2P{0x10000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1PLL3R{0x20000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1LSE{0x30000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1LSI{0x40000000U};
+		constexpr static uint32_t domain2ClockConfig2LPTIM1PerClk{0x50000000U};
+
+		// Domain 4 peripheral clock configuration register constants
+		constexpr static uint32_t domain3ClockConfigLPUART1Mask{0x00000007U};
+		constexpr static uint32_t domain3ClockConfigLPUART1PClkD3{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigLPUART1PLL2Q{0x00000001U};
+		constexpr static uint32_t domain3ClockConfigLPUART1PLL3Q{0x00000002U};
+		constexpr static uint32_t domain3ClockConfigLPUART1HSI{0x00000003U};
+		constexpr static uint32_t domain3ClockConfigLPUART1CSI{0x00000004U};
+		constexpr static uint32_t domain3ClockConfigLPUART1LSE{0x00000005U};
+		constexpr static uint32_t domain3ClockConfigI2C4Mask{0x00000300U};
+		constexpr static uint32_t domain3ClockConfigI2C4PClk4{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigI2C4PLL3R{0x00000100U};
+		constexpr static uint32_t domain3ClockConfigI2C4HSI{0x00000200U};
+		constexpr static uint32_t domain3ClockConfigI2C4CSI{0x00000300U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2Mask{0x00001c00U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2PClk4{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2PLL2P{0x00000400U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2PLL3R{0x00000800U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2LSE{0x00000c00U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2LSI{0x00001000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM2PerClk{0x00001400U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345Mask{0x0000e000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345PClk4{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345PLL2P{0x00002000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345PLL3R{0x00004000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345LSE{0x00006000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345LSI{0x00008000U};
+		constexpr static uint32_t domain3ClockConfigLPTIM345PerClk{0x0000a000U};
+		constexpr static uint32_t domain3ClockConfigADCMask{0x00030000U};
+		constexpr static uint32_t domain3ClockConfigADCPLL2P{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigADCPLL3R{0x00010000U};
+		constexpr static uint32_t domain3ClockConfigADCPerClk{0x00020000U};
+		constexpr static uint32_t domain3ClockConfigSAI4AMask{0x00e00000U};
+		constexpr static uint32_t domain3ClockConfigSAI4APLL1Q{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4APLL2P{0x00200000U};
+		constexpr static uint32_t domain3ClockConfigSAI4APLL3P{0x00400000U};
+		constexpr static uint32_t domain3ClockConfigSAI4AI2SClk{0x00600000U};
+		constexpr static uint32_t domain3ClockConfigSAI4APerClk{0x00800000U};
+		constexpr static uint32_t domain3ClockConfigSAI4ASPDIFRXClk{0x00a00000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BMask{0x07000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BPLL1Q{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BPLL2P{0x01000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BPLL3P{0x02000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BI2SClk{0x03000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BPerClk{0x04000000U};
+		constexpr static uint32_t domain3ClockConfigSAI4BSPDIFRXClk{0x05000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6Mask{0x70000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6PClk4{0x00000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6PLL2Q{0x10000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6PLL3Q{0x20000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6HSI{0x30000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6CSI{0x40000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6HSE{0x50000000U};
+		constexpr static uint32_t domain3ClockConfigSAI6I2CClk{0x60000000U};
 	} // namespace rcc
 
 	namespace pwr
